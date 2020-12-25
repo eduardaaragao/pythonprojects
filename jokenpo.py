ff1b2b4152cfdabde146cd8c7f9838f54ee3fef2
estgv18545@alunos.estgv.ipv.pt <estgv18545@alunos.estgv.ipv.pt>
2020-12-25 13:18:50 +0000

    First commit

---
+++ jokenpo.py
@@ -0,0 +1,34 @@
+from random import randint
+from time import sleep
+print('''Your options:
+
+[0] Rock
+[1] Paper
+[2] Scissors''')
+
+myChoice = int(input('What is your choice? '))
+items = ('Rock', 'Paper', 'Scissors')
+
+if myChoice != 0 and myChoice != 1 and myChoice != 2:
+    print('Choice not accepted')
+else:
+    print('Thinking...')
+    sleep(2)
+    computerChoice = randint(0, 2)
+
+    if computerChoice == myChoice:
+        print('Tie!! Computer also chose', items[computerChoice])
+    else:
+        if myChoice == 0 and computerChoice == 1:
+            print('Computer won!')
+        if myChoice == 0 and computerChoice == 2:
+            print('You won!')
+        if myChoice == 1 and computerChoice == 2:
+            print('Computer won!')
+        if myChoice == 1 and computerChoice == 0:
+            print('You won!')
+        if myChoice == 2 and computerChoice == 1:
+            print('You won!')
+        if myChoice == 2 and computerChoice == 0:
+            print('Computer won!')
+        print('Computer chose {} and you chose' .format(items[computerChoice]), items[myChoice])
